800fd414a0a82d46bd2cdee4c4b6740cc788b05f
jharaujoads2502 <jharaujoads@gmail.com>
2022-11-03 20:01:26 -0300

    exemplos da aula 1

---
+++ type-boolean.js
@@ -0,0 +1,13 @@
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivado = false;
+
+console.log(primeiroNumero === segundoNumero);
+
+
+
+
+const primeiraString = 'Teste';
+const segundaString = 'Teste 2';
+
+console.log(primeiraString === segundaString);
---
+++ type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = '34567';
+
+const citacao = 'Meu nome é ';
+
+const nome = 'José'
+console.log(citacao);
+
+//concatenação (+)
+
+console.log(citacao + nome);
