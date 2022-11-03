c43b41ab057b53e3a9719c7379b44372e4c8eb2e
jharaujoads2502 <jharaujoads@gmail.com>
2022-11-03 20:48:43 -0300

    aula 2

---
+++ conversões.js
@@ -0,0 +1,17 @@
+//tipo de dado
+// booleanos
+
+//conversão implícita 
+
+const numero = 456;
+const numeroString = '456';
+
+
+console.log(numero === numeroString);
+console.log(numero == numeroString);
+
+console.log(numero + numeroString);
+
+console.log(numero + Number(numeroString));
+//conversão explícita 
+
