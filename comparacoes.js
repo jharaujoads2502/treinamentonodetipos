6739256f8d64460fd8a5adc0c562c8d7eb64aefb
jharaujoads2502 <jharaujoads@gmail.com>
2022-11-04 20:40:19 -0300

    aula 3

---
+++ comparacoes.js
@@ -0,0 +1,12 @@
+//== (comparação implicita)
+
+const numero = 3;
+const texto = '3';
+
+console.log(numero == texto);
+console.log(numero === texto);
+
+//typeof
+
+console.log(typeof(numero) + ' ' +typeof(texto));
+
---
+++ templateString.js
@@ -0,0 +1,7 @@
+const nome = 'Jose';
+const idade = 2022-1994;
+const cidadeNascimento = 'Guaxupé'
+const apresentacao = 'meu nome é '+ nome + ' idade ' + idade + ' cidade natal '+ cidadeNascimento;
+const apresentacaoTemp = `meu nome é ${nome} minha idade  é ${idade} cidade natal ${cidadeNascimento}`
+console.log(apresentacao);
+console.log(apresentacaoTemp);
